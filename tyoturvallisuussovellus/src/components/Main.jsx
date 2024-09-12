4dbb5adabdedb50b2d460ddd2063aee31036e289
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-09-12 13:20:47 +0300

    Add code to fetch form from backend

---
+++ tyoturvallisuussovellus/src/components/Main.jsx
@@ -0,0 +1,36 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+import { useState, useEffect } from 'react';
+
+const Main = () => {
+
+    // TODO: uncomment the following part and add correct url
+
+//  const [message, setMessage] = useState('')
+//  const fetchMessage = async () => {
+//    const response = await fetch('<url-here>')
+//    const json = await response.json()
+//
+//    setMessage(json.message)
+//  }
+//  useEffect(() => {
+//    fetchMessage()
+//  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text>Placeholder text</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e1e4e8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Main;
