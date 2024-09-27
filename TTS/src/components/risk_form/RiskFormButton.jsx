cc57cb2217f3fa2b4d5d8dc641f1bbb5bdf29492
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-09-27 18:51:24 +0300

    Fix lint issue

--- TTS/src/components/risk_form/RiskFormButton.jsx
+++ TTS/src/components/risk_form/RiskFormButton.jsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigate } from 'react-router-native';
 
 const RiskFormButton = ({ title, worksite }) => {
