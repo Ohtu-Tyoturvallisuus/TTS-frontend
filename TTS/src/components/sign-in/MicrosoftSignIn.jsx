92bacbbb3dc2107ed29796982a2d04ddd36b26ff
Soki <soki.fin02@gmail.com>
2024-11-02 11:12:27 +0200

    Adjust text size for Microsoft Sign-In button

--- TTS/src/components/sign-in/MicrosoftSignIn.jsx
+++ TTS/src/components/sign-in/MicrosoftSignIn.jsx
@@ -100,7 +100,7 @@ const MicrosoftSignIn = () => {
           onPress={() => promptAsync()} 
           className="bg-[#ef7d00] rounded-lg justify-center items-center py-4 px-6 w-3/4 my-2"
         >
-          <Text className="text-white text-base font-bold">{t('microsoftsignin.signInText')}</Text>
+          <Text className="text-white font-bold">{t('microsoftsignin.signInText')}</Text>
         </TouchableOpacity>
       )}
     </View>
