796f128cb501b4343cee574136ca53fa136711de
latoma <127959432+latoma@users.noreply.github.com>
2024-11-06 13:30:11 +0200

    Add TextTranslations and implement language selection components

--- TTS/src/components/CountryFlag.jsx
+++ TTS/src/components/CountryFlag.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import OriginalCountryFlag from 'react-native-country-flag';
 
-const CountryFlag = ({ isoCode, size }) => {
+const CountryFlag = ({ isoCode, size, style }) => {
   return (
     <OriginalCountryFlag
       isoCode={isoCode}
       size={size}
-      style={{ borderColor: '#c5c6c8', borderWidth: 1, borderRadius: 3 }}
+      style={{ 
+        borderColor: '#c5c6c8', 
+        borderWidth: 1, 
+        borderRadius: 3, 
+        ...style 
+      }}
     />
   );
 };
