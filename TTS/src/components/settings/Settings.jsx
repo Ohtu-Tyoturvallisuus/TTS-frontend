47f048c87d4811a4cc358a7f494a7114957a09f7
Soki <soki.fin02@gmail.com>
2024-11-25 12:44:23 +0200

    remove join survey button

--- TTS/src/components/settings/Settings.jsx
+++ TTS/src/components/settings/Settings.jsx
@@ -9,7 +9,6 @@ import ChangeLanguage from './ChangeLanguage';
 import CloseButton from '@components/buttons/CloseButton';
 import SettingsButton from '@components/buttons/SettingsButton';
 import MyObservations from './MyObservations';
-import JoinSurvey from '@components/risk-form/JoinSurvey';
 
 const Settings = () => {
   const [changeLanguageVisible, setChangeLanguageVisible] = useState(false);
@@ -48,7 +47,6 @@ const Settings = () => {
     <View style={styles.container}>
       <Text style={styles.text}>{username}</Text>
       <Text style={styles.text}>{email}</Text>
-      {username && <JoinSurvey />}
       {username && <MyObservations />}
       <SettingsButton
         onPress={() => setChangeLanguageVisible(true)}
