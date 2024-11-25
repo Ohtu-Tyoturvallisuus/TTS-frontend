479683f5f5c21a1a111611d4c53ebfbcb65c08d6
latoma <127959432+latoma@users.noreply.github.com>
2024-11-25 15:57:49 +0200

    Seperate task info code from riskform

--- TTS/src/components/risk-form/RiskForm.jsx
+++ TTS/src/components/risk-form/RiskForm.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useContext, useEffect, useRef } from 'react';
-import { View, TextInput, ScrollView, Text, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
-import SectionedMultiSelect from 'react-native-sectioned-multi-select';
-import { MaterialIcons as Icon } from '@expo/vector-icons';
+import { View, ScrollView, Text, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useTranslation } from 'react-i18next';
 
@@ -12,34 +10,31 @@ import { useFormContext } from '@contexts/FormContext';
 import { useTranslationLanguages } from '@contexts/TranslationContext';
 import { UserContext } from '@contexts/UserContext';
 import { NavigationContext } from '@contexts/NavigationContext';
-import { useScaffoldItems } from '@utils/scaffoldUtils';
 import RiskNote from './RiskNote';
-import MultiChoiceButtonGroup from '@components/buttons/MultiChoiceButtonGroup';
 import CloseButton from '@components/buttons/CloseButton';
 import SuccessAlert from '@components/SuccessAlert';
 import Loading from '@components/Loading';
 import ConfirmationModal from '@components/ConfirmationModal';
-import SpeechToTextView from '@components/speech-to-text/SpeechToTextView';
 import FilledRiskForm from './FilledRiskForm';
-import SelectTranslateLanguage from '@components/speech-to-text/SelectTranslateLanguage';
+import TaskInfo from './TaskInfo';
 
 const RiskForm = () => {
-  const { 
-    selectedProject: project, 
+  const {
+    selectedProject: project,
     selectedSurveyURL: surveyURL,
     resetProjectAndSurvey,
   } = useContext(ProjectSurveyContext);
 
-  const { 
-    formData, 
-    updateFormField, 
+  const {
+    formData,
+    updateFormField,
     replaceFormData,
-    task, 
-    setTask, 
-    scaffoldType, 
-    setScaffoldType, 
-    taskDesc, 
-    setTaskDesc, 
+    task,
+    setTask,
+    scaffoldType,
+    setScaffoldType,
+    taskDesc,
+    setTaskDesc,
   } = useFormContext();
 
   const { setCurrentLocation } = useContext(NavigationContext);
@@ -56,7 +51,7 @@ const RiskForm = () => {
   const { surveyData, loading, error } = useFetchSurveyData(surveyURL);
 
   // Merges previous survey's data to the form if surveyData is available
-  useEffect(() => {    
+  useEffect(() => {
     if (surveyData) {
       const currentNotes = surveyData.risk_notes.reduce((acc, note) => {
         acc[note.note] = {
@@ -72,7 +67,7 @@ const RiskForm = () => {
         console.log("Merging prev survey's data");
         replaceFormData(currentNotes);
       }
-  
+
       setTask(surveyData.task);
       setScaffoldType(surveyData.scaffold_type);
       setTaskDesc(surveyData.description);
@@ -125,16 +120,14 @@ const RiskForm = () => {
     updateFormField(newKey, 'risk_type', type);
   };
 
-  const scaffoldItems = useScaffoldItems();
-
   if (loading || error) {
     return <Loading loading={loading} error={error} title={t('riskform.loadingFormData')} />;
   }
 
   return (
     <View className="flex items-center justify-center">
-      <ScrollView 
-        className="bg-white flex-grow p-5 w-full h-full" 
+      <ScrollView
+        className="bg-white flex-grow p-5 w-full h-full"
         contentContainerStyle={{ paddingBottom: 30 }}
       >
         <Image
@@ -144,74 +137,17 @@ const RiskForm = () => {
         {!submitted ? (
           <>
             <Text className="text-2xl font-bold pb-5 text-center">{t('riskform.title')}</Text>
-          
             {error && <Text>{t('riskform.errorFetchingData')}</Text>}
             {/* Projektin tiedot */}
             {project ? (
-              <View className="mb-3">
-              <SelectTranslateLanguage setTranslationLanguages={setToLangs} />
-
-                <Text className="text-lg font-bold py-2">{t('riskform.projectName')}:</Text>
-                <Text>{project.project_name}</Text>
-            
-                <Text className="text-lg font-bold py-2">{t('riskform.projectId')}: </Text>
-                <Text>{project.project_id}</Text>
-                <Text className="text-lg font-bold py-2">{t('riskform.task')}:</Text>
-                <MultiChoiceButtonGroup 
-                  options={['installation', 'modification', 'dismantling']} 
-                  selectedValues={task}
-                  onChange={(value) => setTask(value)}
-                  renderOption={(option) => t(`riskform.${option}`)}
+                <TaskInfo
+                  project={project}
+                  setToLangs={setToLangs}
                 />
-  
-                <Text className="text-lg font-bold py-2">{t('riskform.scaffoldType')}:</Text>
-                <View>
-                  <SectionedMultiSelect
-                    items={scaffoldItems}
-                    IconRenderer={Icon}
-                    uniqueKey="id"
-                    onSelectedItemsChange={setScaffoldType}
-                    selectedItems={scaffoldType}
-                    selectText={t('riskform.chooseScaffoldType')}
-                    selectedText={t('riskform.selected')}
-                    confirmText={t('general.confirm')}
-                    hideSearch
-                    colors={{
-                      success: '#ef7d00'
-                    }}
-                    styles={{
-                      button: {
-                        backgroundColor: '#ef7d00',
-                        borderRadius: 8,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        paddingVertical: 12,
-                        paddingHorizontal: 24,
-                        width: '100%',
-                        marginVertical: 8,
-                      },
-                    }}
-                  />
-                </View>
-  
-                <Text className="text-lg font-bold py-2">{t('riskform.taskDescription')}:</Text>
-                <TextInput
-                  testID='taskDesc'
-                  className="border border-gray-300 rounded p-2 h-24"
-                  value={taskDesc}
-                  onChangeText={(value) => setTaskDesc(value)}
-                  multiline={true}
-                  textAlignVertical="top"
-                />
-                <SpeechToTextView
-                  setDescription={setTaskDesc}
-                  translate={false}
-                />
-              </View>
             ) : (
               <Text>{t('riskform.noProject')}</Text>
             )}
-  
+
             <Text className="border-b border-gray-300 text-xl font-bold pb-1 text-center">
               {t('riskform.scaffoldRisks')}
             </Text>
@@ -234,19 +170,19 @@ const RiskForm = () => {
               )
             }
 
-            <TouchableOpacity 
-              className="p-2 border border-green-500 rounded my-2 min-h-12 items-center" 
+            <TouchableOpacity
+              className="p-2 border border-green-500 rounded my-2 min-h-12 items-center"
               onPress={() => addNewRiskNote('riskform.otherScaffolding', 'scaffolding')}
             >
               <Text className="text-green-500 text-lg font-bold">+ {t('riskform.otherScaffolding')}</Text>
             </TouchableOpacity>
-          
+
             <Text className="border-b border-gray-300 text-xl font-bold pb-1 text-center mt-5">
               {t('riskform.environmentRisks')}
             </Text>
             {Object.entries(formData)
               .filter(([, value]) => value.risk_type === 'environment')
-              .map(([key]) => 
+              .map(([key]) =>
                 key.startsWith('riskform.otherEnvironment') ? (
                   <RiskNote
                     key={key}
@@ -262,13 +198,13 @@ const RiskForm = () => {
                 )
               )
             }
-            <TouchableOpacity 
-              className="p-2 border border-green-500 rounded my-2 min-h-12 items-center" 
+            <TouchableOpacity
+              className="p-2 border border-green-500 rounded my-2 min-h-12 items-center"
               onPress={() => addNewRiskNote('riskform.otherEnvironment', 'environment')}
             >
               <Text className="text-green-500 text-lg font-bold">+ {t('riskform.otherEnvironment')}</Text>
             </TouchableOpacity>
-          
+
             {project && (
               <>
                 <FilledRiskForm
@@ -292,9 +228,9 @@ const RiskForm = () => {
         )}
       </ScrollView>
       {showSuccessAlert && (
-        <SuccessAlert 
-          message={t('riskform.successMessage')} 
-          onDismiss={handleClose} 
+        <SuccessAlert
+          message={t('riskform.successMessage')}
+          onDismiss={handleClose}
         />
       )}
       <ConfirmationModal
