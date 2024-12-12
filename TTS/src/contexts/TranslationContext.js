19b187a04a4104ce470152e4e9f3ca3d37764c57
latoma <127959432+latoma@users.noreply.github.com>
2024-12-12 13:31:28 +0200

    Add english as default translation language

--- TTS/src/contexts/TranslationContext.js
+++ TTS/src/contexts/TranslationContext.js
@@ -6,15 +6,31 @@ const TranslationContext = createContext();
 export const TranslationProvider = ({ children }) => {
   const { i18n } = useTranslation();
   const [fromLang, setFromLang] = useState(i18n.language.substring(0, 2));
-  const [toLangs, setToLangs] = useState([]);
+  const [toLangs, setToLangs] = useState(fromLang !== 'en' ? ['en'] : []);
 
   useEffect(() => {
     console.log(`TranslationContext language set to ${i18n.language.substring(0, 2)}`);
     setFromLang(i18n.language.substring(0, 2));
+    clearToLangs();
   }, [i18n.language]);
 
+  const clearToLangs = () => {
+    console.log('Clearing toLangs, current fromlang:', i18n.language.substring(0, 2));
+    if ( i18n.language.substring(0, 2) !== 'en' ) {
+      setToLangs(['en']);
+    } else {
+      setToLangs([]);
+    }
+  };
+
   return (
-    <TranslationContext.Provider value={{ fromLang, setFromLang, toLangs, setToLangs }}>
+    <TranslationContext.Provider value={{
+      fromLang,
+      setFromLang,
+      toLangs,
+      setToLangs,
+      clearToLangs
+      }}>
       {children}
     </TranslationContext.Provider>
   );
